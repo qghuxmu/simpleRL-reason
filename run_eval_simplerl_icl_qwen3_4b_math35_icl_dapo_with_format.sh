81506d0752c7270abaa0dd8a882129f516b749ba
qghuxmu <limazhiluyao@gmail.com>
2025-06-21 11:34:10 -0700

    update

---
+++ run_eval_simplerl_icl_qwen3_4b_math35_icl_dapo_with_format.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+huggingface-cli download Qwen/Qwen3-4B-Base --local-dir /workspace/Qwen3-4B-Base
+
+WANDB_MODE=offline bash eval_math_nodes.sh \
+    --run_name /mnt/yaoxiang-wang-yeyuninterns-out/qingguo/simplerl_icl/qwen3_4b_math35_icl_dapo_with_format \
+    --init_model /workspace/Qwen3-4B-Base \
+    --template qwen-boxed \
+    --tp_size 1 \
+    --add_step_0 true \
+    --temperature 1.0 \
+    --top_p 0.95 \
+    --max_tokens 16000 \
+    --benchmarks aime24,amc23,math500,olympiadbench,gsm8k,minerva_math \
+    --n_sampling 1 
