04dc023243cd7a4c0016721f6b7fee2cd5cf16f2
qghuxmu <limazhiluyao@gmail.com>
2025-06-21 20:27:45 -0700

    update

--- run_eval_simplerl_icl_qwen3_4b_math35_icl.sh
+++ run_eval_simplerl_icl_qwen3_4b_math35_icl.sh
@@ -8,7 +8,7 @@ WANDB_MODE=offline bash eval_math_nodes.sh \
     --init_model /workspace/Qwen3-4B-Base \
     --template qwen-boxed \
     --tp_size 1 \
-    --add_step_0 true \
+    --add_step_0 false \
     --temperature 1.0 \
     --top_p 0.95 \
     --max_tokens 16000 \
